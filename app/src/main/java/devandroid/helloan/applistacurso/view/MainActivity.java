21dd8314504b256b0a46cceb2d1c713e9272116a
Helloan <helloan.cardoso98@gmail.com>
2023-05-24 16:16:28 -0300

    correções no layout2

---
+++ app/src/main/java/devandroid/helloan/applistacurso/view/MainActivity.java
@@ -0,0 +1,16 @@
+package devandroid.helloan.applistacurso.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import devandroid.helloan.applistacurso.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
