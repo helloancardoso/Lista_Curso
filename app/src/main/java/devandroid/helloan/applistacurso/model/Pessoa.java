e9ee700f4ed13be9f9bcde60e0ec94b7fe126940
Helloan <helloan.cardoso98@gmail.com>
2023-05-25 10:25:37 -0300

    Uso dos métodos get e set.

--- app/src/main/java/devandroid/helloan/applistacurso/model/Pessoa.java
+++ app/src/main/java/devandroid/helloan/applistacurso/model/Pessoa.java
@@ -4,6 +4,55 @@ package devandroid.helloan.applistacurso.model;
 
 
 public class Pessoa {
+
+    // construtor (classe -> pessoa)
+
+    //atributos -> objetos -> molde -> modelo -> template
+
+    //métodos de acesso - Getter and Setter
+
+    //Primeiro Nome <- a mesma coisa, porém tem que seguir a ordem padrao sendo orientada objeto java
+
+    private String primeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+    private String telefoneContato;
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
+
+
+
     public Pessoa(){
 
 
--- app/src/main/java/devandroid/helloan/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/helloan/applistacurso/view/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;      // <- pascalCase - camelCase
+    String dadosOutraPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        // atribuir conteudo, dados, valores para o objeto
+        // conforme seu modelo = template ( usando get and set )
+
+        pessoa.setPrimeiroNome("Helloan");
+        pessoa.setSobreNome("Cardoso");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("21-997482790");
+
+
+        outraPessoa=new Pessoa();
+
+        outraPessoa.setPrimeiroNome("Luis");
+        outraPessoa.setSobreNome("Alves");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("21-91542789");
+
+        dadosPessoa="Primeiro nome: ";
+        dadosPessoa+=pessoa.getPrimeiroNome();
+        dadosPessoa+=" Sobrenome: ";
+        dadosPessoa+=pessoa.getSobreNome();
+        dadosPessoa+=" Curso Desejado: ";
+        dadosPessoa+=pessoa.getCursoDesejado();
+        dadosPessoa+=" Telefone de Contato: ";
+        dadosPessoa+=pessoa.getTelefoneContato();
+
+
+        dadosOutraPessoa="Primeiro nome: ";
+        dadosOutraPessoa+=outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa+=" Sobrenome: ";
+        dadosOutraPessoa+=outraPessoa.getSobreNome();
+        dadosOutraPessoa+=" Curso Desejado: ";
+        dadosOutraPessoa+=outraPessoa.getCursoDesejado();
+        dadosOutraPessoa+=" Telefone de Contato: ";
+        dadosOutraPessoa+=outraPessoa.getTelefoneContato();
+
+        int parada=0;
 
 
     }
